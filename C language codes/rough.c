fe2feccf7200f40cb0af2e6978291e31159225b3
VimalKanzariya2307 <vimalkanzariya80@gmail.com>
2025-04-03 15:14:59 +0530

    added some files

--- C language codes/rough.c
+++ C language codes/rough.c
@@ -537,18 +537,81 @@ int main()
  */
 
 
- #include<stdio.h>
- void sum();
- int main()
- {
-    sum();
+//  #include<stdio.h>
+//  void sum();
+//  int main()
+//  {
+//     sum();
+//     return 0;
+//  }
+//  void sum()
+//  {
+//     int num1,num2,result;
+//     printf("enter your num:");
+//     scanf("%d %d",&num1,&num2);
+//     result = num1+num2;
+//     printf("your ans is : %d ",result);
+//  }
+
+//without return type and without argument function
+
+#include <stdio.h>
+void Sum(); 
+int main() {
+    Sum(); 
     return 0;
- }
- void sum()
- {
-    int num1,num2,result;
-    printf("enter your num:");
-    scanf("%d %d",&num1,&num2);
-    result = num1+num2;
-    printf("your ans is : %d ",result);
- }
+}
+void Sum() { 
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    printf("Sum: %d\n", num1 + num2);
+}
+
+//with return type and without argument function
+
+#include <stdio.h>
+int Sum2();
+int main() {
+    int result = Sum2(); 
+    printf("Sum: %d\n", result);
+    return 0;
+}
+int Sum2() { 
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    return num1 + num2; 
+}
+
+//without return type and with argument function
+
+#include <stdio.h>
+void Sum3(int a, int b);
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    Sum3(num1, num2); 
+    return 0;
+}
+void Sum3(int a, int b) { 
+    printf("Sum: %d\n", a + b); 
+}
+
+//with return type and with argument function
+
+#include <stdio.h>
+int Sum4(int a, int b);
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    int result = Sum4(num1, num2); 
+    printf("Sum: %d\n", result);
+    return 0;
+}
+int Sum4(int a, int b) { 
+    return a + b; 
+}
+
